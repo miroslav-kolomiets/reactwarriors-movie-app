43137f97a8111b18791a6354f678f030d654fac3
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2019-09-23 15:04:11 +0300

    Base code from videos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,61 @@
-import React from "react";
-import Filters from "./Filters/Filters";
-import MoviesList from "./Movies/MoviesList";
+import React from 'react';
+import Filters from './Filters/Filters';
+import MoviesList from './Movies/MoviesList';
 
 export default class App extends React.Component {
-  render() {
+  constructor () {
+    super ();
+
+    this.state = {
+      filters: {
+        sort_by: 'popularity.desc',
+      },
+      page: 1,
+    };
+  }
+
+  onChangeFilters = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const newFilters = {
+      ...this.state.filters,
+      [name]: value,
+    };
+    this.setState ({
+      filters: newFilters,
+    });
+  };
+
+  onChangePage = page => {
+    this.setState ({
+      page,
+    });
+  };
+
+  render () {
+    const {filters, page} = this.state;
     return (
       <div className="container">
         <div className="row mt-4">
           <div className="col-4">
-            <div className="card" style={{ width: "100%" }}>
+            <div className="card" style={{width: '100%'}}>
               <div className="card-body">
                 <h3>Фильтры:</h3>
-                <Filters />
+                <Filters
+                  page={page}
+                  filters={filters}
+                  onChangePage={this.onChangePage}
+                  onChangeFilters={this.onChangeFilters}
+                />
               </div>
             </div>
           </div>
           <div className="col-8">
-            <MoviesList />
+            <MoviesList
+              page={page}
+              filters={this.state.filters}
+              onChangePage={this.onChangePage}
+            />
           </div>
         </div>
       </div>
--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -1,34 +1,52 @@
-import React, { Component } from "react";
-import MovieItem from "./MovieItem";
-import { API_URL, API_KEY_3 } from "../../api/api";
+import React, {Component} from 'react';
+import MovieItem from './MovieItem';
+import {API_URL, API_KEY_3} from '../../api/api';
 
 export default class MovieList extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super ();
 
     this.state = {
-      movies: []
+      movies: [],
     };
   }
 
-  componentDidMount() {
-    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU`;
-    fetch(link)
-      .then(response => {
-        return response.json();
+  getMovies = (filters, page) => {
+    const {sort_by} = filters;
+    const link = `${API_URL}/discover/movie?api_key=${API_KEY_3}&language=ru-RU&sort_by=${sort_by}&page=${page}`;
+
+    fetch (link)
+      .then (response => {
+        return response.json ();
       })
-      .then(data => {
-        this.setState({
-          movies: data.results
+      .then (data => {
+        this.setState ({
+          movies: data.results,
         });
       });
+  };
+
+  componentDidMount () {
+    this.getMovies (this.props.filters, this.props.page);
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidUpdate (prevProps) {
+    if (this.props.filters.sort_by !== prevProps.filters.sort_by) {
+      this.getMovies (this.props.filters, 1);
+      this.props.onChangePage (1);
+    }
+
+    if (this.props.page !== prevProps.page) {
+      this.getMovies (this.props.filters, this.props.page);
+    }
+  }
+
+  render () {
+    const {movies} = this.state;
+    console.log ('filters ', this.props.filters);
     return (
       <div className="row">
-        {movies.map(movie => {
+        {movies.map (movie => {
           return (
             <div key={movie.id} className="col-6 mb-4">
               <MovieItem item={movie} />
