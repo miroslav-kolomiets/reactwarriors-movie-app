1ae4efbf94f03f8270e3e7b20161bbb78775fce2
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2019-09-29 19:44:23 +0300

    Updates

---
+++ precache-manifest.39f4ffbef72883e71b065b10df4a0c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172b7cfe09d1c46e661e5867d4bb0620",
+    "url": "/reactwarriors-movie-app/index.html"
+  },
+  {
+    "revision": "ba1ba5647b1999918297",
+    "url": "/reactwarriors-movie-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3d76e563b331e93734ca",
+    "url": "/reactwarriors-movie-app/static/css/main.f8eda838.chunk.css"
+  },
+  {
+    "revision": "ba1ba5647b1999918297",
+    "url": "/reactwarriors-movie-app/static/js/2.7e50e38c.chunk.js"
+  },
+  {
+    "revision": "3d76e563b331e93734ca",
+    "url": "/reactwarriors-movie-app/static/js/main.990c28b7.chunk.js"
+  },
+  {
+    "revision": "ea6b2d9efeaf5d262dd1",
+    "url": "/reactwarriors-movie-app/static/js/runtime~main.bb82fc82.js"
+  }
+]);
