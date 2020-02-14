eed196dcd9e94e4c6fec2c69b59f8e33400fe1b0
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2020-02-14 18:16:10 +0200

    Rewiev changes

--- src/components/Movies/Watchlist.jsx
+++ src/components/Movies/Watchlist.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { API_URL, API_KEY_3, fetchApi } from '../../api/api';
+import CallApi from '../../api/api';
 import IconButton from '@material-ui/core/IconButton';
-import { Bookmark, BookmarkBorder } from '@material-ui/icons';
-import { AppContext } from '../App';
-import { Modal, ModalBody } from 'reactstrap';
-import LoginForm from '../Header/Login/LoginForm';
+import {Bookmark, BookmarkBorder} from '@material-ui/icons';
+import AppContextHOC from '../HOC/AppContextHOC';
 
 class Watchlist extends React.Component {
   constructor() {
@@ -12,79 +10,40 @@ class Watchlist extends React.Component {
 
     this.state = {
       isInWatchlist: null,
-      showModal: false,
     };
   }
 
-  toggleModal = () => {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal,
-    }));
-  };
-
-  addToFav = (item, session_id) => {
+  toggleWatchlist = (id, session_id, isInWatchlist) => {
     if (session_id) {
-      // console.log(item);
-      fetchApi(
-        `${API_URL}/account/7933447/watchlist?api_key=${API_KEY_3}&session_id=${session_id}`,
-        {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-type': 'application/json',
+      CallApi.post(
+        `/account/${this.props.user.id}/watchlist`, {
+          params: {
+            session_id: session_id
           },
-          body: JSON.stringify({
+          body: {
             media_type: 'movie',
-            media_id: item.id,
-            watchlist: true,
-          }),
+            media_id: id,
+            watchlist: isInWatchlist,
+          }
         }
       ).then(() => {
         this.setState({
-          isInWatchlist: true,
+          isInWatchlist,
         });
       });
     } else {
-      this.toggleModal();
-    }
-  };
-
-  removeFromFav = (item, session_id) => {
-    if (session_id) {
-      // console.log(item);
-      fetchApi(
-        `${API_URL}/account/7933447/watchlist?api_key=${API_KEY_3}&session_id=${session_id}`,
-        {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            media_type: 'movie',
-            media_id: item.id,
-            watchlist: false,
-          }),
-        }
-      ).then(() => {
-        this.setState({
-          isInWatchlist: false,
-        });
-      });
-    } else {
-      this.toggleModal();
+      this.props.toggleModal();
     }
   };
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (!(this.props.watchlistMovies === prevProps.watchlistMovies)) {
-      const { item, watchlistMovies } = this.props;
+      const {id, watchlistMovies} = this.props;
       if (watchlistMovies) {
         let watchlistMoviesIds = watchlistMovies.map(movie => {
           return movie.id;
         });
-        // console.log(watchlistMoviesIds);
-        if (watchlistMoviesIds.includes(item.id)) {
+        if (watchlistMoviesIds.includes(id)) {
           this.setState({
             isInWatchlist: true,
           });
@@ -98,55 +57,31 @@ class Watchlist extends React.Component {
   }
 
   render() {
-    const { item, session_id, user } = this.props;
-    const { isInWatchlist } = this.state;
+    const {id, session_id} = this.props;
+    const {isInWatchlist} = this.state;
 
     if (isInWatchlist) {
       return (
         <div>
           <IconButton
             aria-label=""
-            onClick={() => this.removeFromFav(item, session_id)}
+            onClick={() => this.toggleWatchlist(id, session_id, false)}
           >
-            <BookmarkBorder />
+            <BookmarkBorder/>
           </IconButton>
         </div>
       );
     } else {
       return (
-        <div>
-          {user ? null : (
-            <Modal isOpen={this.state.showModal} toggle={this.toggleModal}>
-              <ModalBody>
-                <LoginForm
-                  updateUser={this.props.updateUser}
-                  updateSessionId={this.props.updateSessionId}
-                />
-              </ModalBody>
-            </Modal>
-          )}
-          <IconButton
-            aria-label=""
-            onClick={() => this.addToFav(item, session_id)}
-          >
-            <Bookmark />
-          </IconButton>
-        </div>
+        <IconButton
+          aria-label=""
+          onClick={() => this.toggleWatchlist(id, session_id, true)}
+        >
+          <Bookmark/>
+        </IconButton>
       );
     }
   }
 }
 
-const WatchlistContainer = props => {
-  return (
-    <AppContext.Consumer>
-      {context => {
-        return <Watchlist {...context} {...props} />;
-      }}
-    </AppContext.Consumer>
-  );
-};
-
-WatchlistContainer.displayName = 'WatchlistContainer';
-
-export default WatchlistContainer;
+export default AppContextHOC(Watchlist);
