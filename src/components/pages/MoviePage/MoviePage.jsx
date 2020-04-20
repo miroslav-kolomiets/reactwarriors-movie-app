7087f19532203cd6a247fdac50a78d6d87ddf18e
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2020-04-20 12:48:32 +0300

    Add movie page

--- src/components/pages/MoviePage/MoviePage.jsx
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import {NavLink, Route, Switch} from "react-router-dom";
+import {Nav, NavItem, TabContent,} from 'reactstrap';
 import CallApi from '../../../api/api';
+import MovieDetail from "./MovieDetail";
+import MovieCredits from "./MovieCredits";
+import MovieVideos from "./MovieVideos";
+import MovieOverview from './MovieOverview';
 
 export default class MoviePage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {}
+  }
+
   componentDidMount() {
-    CallApi.get(`/movie/${this.props.match.params.id}`)
+    CallApi.get(`/movie/${this.props.match.params.id}`).then(data => {
+      this.setState({data});
+    })
   }
 
   render() {
-    console.log(this.props);
-    return <div>Movie Page</div>;
+    const path = this.props.match.url;
+    return (
+      <div className='movie-page'>
+        <MovieOverview movie={this.state.data}/>
+        <Nav tabs>
+          <NavItem>
+            <NavLink to={`${path}/details`} className="link">
+              details
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to={`${path}/videos`} className="link">
+              videos
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to={`${path}/credits`} className="link">
+              credits
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={1}>
+          <Switch>
+            <Route path={`${path}/details`} component={MovieDetail}/>
+            <Route path={`${path}/videos`} component={MovieVideos}/>
+            <Route path={`${path}/credits`} component={MovieCredits}/>
+          </Switch>
+        </TabContent>
+      </div>
+    )
   }
 };
